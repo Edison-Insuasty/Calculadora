f32837f49441383cfdff15012f79d4d40c73a694
Edison <edisonins15@gmail.com>
2023-06-29 13:36:22 -0500

    Función temperatura

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function calc() {
 // Reset button
 function reset() {
     resultado.value = "";
+    resultado2.value = "";
 }
 
 // Del button
@@ -127,4 +128,29 @@ function myFunction_set(val) {
         theme.dark();
     }
 
+}
+
+///////////
+
+// let celsius = document.getElementById("celsius");
+// let fahrenheit = document.getElementById("fahrenheit");
+
+// function celToFar() {
+//     let output = (parseFloat(celsius.value) * 9 / 5) + 32;
+//     fahrenheit.value = parseFloat(output.toFixed(2));
+// }
+
+// function farToCel() {
+//     let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
+//     celsius.value = parseFloat(output.toFixed(2));
+//     console.log(output);
+// }
+
+
+function convertir() {
+    var celsius = document.getElementById("resultado").value;
+    var fahrenheit = ((celsius * 9) / 5) + 32;
+    resultado2.value = eval(fahrenheit)
+
+    // alert(fahrenheit + "F");
 }
